4ff4d3670f88fe41b77bc71b949da6bd2ff2ba8e
devi <devi.jeevan@gmail.com>
2024-09-18 11:08:06 +0530

    final webapp

--- webapp.py
+++ webapp.py
@@ -2,6 +2,7 @@ import streamlit as st
 import cfunctions
 import os
 
+
 if not os.path.exists("todos.txt"):
     with open("todos.txt","w") as file:
         pass
@@ -17,8 +18,15 @@ st.title("My Todo App")
 st.subheader("Helps organise my daily activities")
 st.write("I write to do tasks everyday")
 
-for items in todos:
-    st.checkbox(items)
+for items ,todo in enumerate(todos):
+    checkbox_value = st.checkbox(todo,key=todo)
+    if  checkbox_value:
+        todos.pop(items)
+        cfunctions.write_todos(todos)
+        del st.session_state[todo]
+      # st.experimental_rerun() #not working in streamlit 1.38 not able to downgrade to 1.37
 
-st.text_input(label="",placeholder="Enter to do",on_change=add_todo,key='web_newtodo')
-# st.text_input(label="",placeholder="Enter to do",onchange=add_todo(),key='web_newtodo')
+st.text_input(label=" ",placeholder="Enter to do",on_change=add_todo,key='web_newtodo')
+
+#to show the session state
+#st.session_state
